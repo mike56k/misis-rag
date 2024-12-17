51cb6463fead42dd3a90e28efcd65746f1bb809a
Ilyaant <antonov.ia240701@gmail.com>
2024-12-18 02:40:13 +0300

    use qwen2.5:3b

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
 # Start Ollama in the background.
 /bin/ollama serve &
 # Record Process ID.
 pid=$!
-
 # Pause for Ollama to start.
 sleep 5
-
 echo "🔴 Retrieve models..."
 ollama pull mxbai-embed-large:latest
-ollama pull mistral:latest
+ollama pull qwen2.5:3b
 echo "🟢 Done!"
-
 # Wait for Ollama process to finish.
 wait $pid
